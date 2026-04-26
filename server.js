c02ff4b466958ed9dbe83efc1779278af3782851
AllenCodess <Aph296@protonmail.com>
2026-04-25 23:43:42 -0400

    added some routes and checking them with postman

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import http from "http";
 const PORT = process.env.PORT;
 const server = http.createServer((req, res) => {
-  // res.setHeader("Content-Type", "text/html");
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>Allen</h1>");
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>HomePage</h1>");
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>About Page</h1>");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end("<h1>PAGE NOT FOUND</h1>");
+  }
 });
 
 server.listen(PORT, () => {
