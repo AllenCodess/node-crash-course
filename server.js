2f87d10d2c107846d2697aeab14ee0a0ef8a4caa
AllenCodess <Aph296@protonmail.com>
2026-04-26 10:39:29 -0400

    loading files instead of running html from res.write

--- server.js
+++ server.js
@@ -1,21 +1,34 @@
 import http from "http";
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
 const PORT = process.env.PORT;
-const server = http.createServer((req, res) => {
+
+// Get Current Path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__filename, __dirname);
+
+const server = http.createServer(async (req, res) => {
   try {
+    let filePath;
     if (req.method === "GET") {
       if (req.url === "/") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end("<h1>HomePage</h1>");
+        filePath = path.join(__dirname, "public", "index.html");
       } else if (req.url === "/about") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end("<h1>About Page</h1>");
+        filePath = path.join(__dirname, "public", "about.html");
       } else {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        res.end("<h1>PAGE NOT FOUND</h1>");
+        filePath = path.join(__dirname, "public", "NotFound.html");
       }
     } else {
       throw new Error("Method not allowed");
     }
+
+    const data = await fs.readFile(filePath);
+    res.setHeader("Content-Type", "text/html");
+    res.write(data);
+    res.end();
   } catch (error) {
     res.writeHead(500, { "Content-Type": "text/plain" });
     res.end(`${error.message}`);
