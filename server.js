a52f0be5a7154639ccbdbaf03a6c7af46953723c
AllenCodess <Aph296@protonmail.com>
2026-04-26 00:03:17 -0400

    error handling routes

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 import http from "http";
 const PORT = process.env.PORT;
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("<h1>HomePage</h1>");
-  } else if (req.url === "/about") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("<h1>About Page</h1>");
-  } else {
-    res.writeHead(404, { "Content-Type": "text/html" });
-    res.end("<h1>PAGE NOT FOUND</h1>");
+  try {
+    if (req.method === "GET") {
+      if (req.url === "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>HomePage</h1>");
+      } else if (req.url === "/about") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>About Page</h1>");
+      } else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("<h1>PAGE NOT FOUND</h1>");
+      }
+    } else {
+      throw new Error("Method not allowed");
+    }
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end(`${error.message}`);
   }
 });
 
